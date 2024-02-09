8c74e823cf6c74d771a85c88c8205c56afa1c601
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-10 00:12:46 +0200

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Created by: <a href="https://github.com/HendMohammed90" target="_blank" rel="noreferrer">Hend Mohammed</a> © 2024 Happy to Help ^_^</p>
+    </div>
+  )
+}
+
+export default Footer
