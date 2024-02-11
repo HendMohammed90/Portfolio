0ba95f19940b4b38f78d44efc9265db6dabab7cb
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-11 21:28:55 +0200

    fix error in MobileNav'

--- src/components/Navbar/MobileNav/MobileNav.jsx
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -5,7 +5,7 @@ import NavListItem from "../NavListItem/NavListItem";
 const MobileNav = ({ isOpen, toggleMenu }) => {
     return (
         <>
-            <div className={`mobile-menu ${isOpen ? "active" : ""}`} onClick={{ toggleMenu }}>
+            <div className={`mobile-menu ${isOpen ? "active" : ""}`} onClick={ toggleMenu }>
                 <div className="mobile-menu-container">
                     <NavListItem />
                 </div>
