dc20405aab16d06228a686cd813d47c3e0a25b27
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-13 17:58:25 +0200

    add work samples section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills/Skills';
 import Experience from './components/WorkExperience/Experience';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import WorkSamples from './components/WorkSamples/WorkSamples';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Skills />
       <Experience />
+      <WorkSamples />
       <Contact />
     </div>
     <Footer />
---
+++ src/components/WorkSamples/WorkSamples.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./WorkSamples.css";
+
+const WorkSamples = () => {
+  return (
+    <section className="work-samples-container">
+      <h5>WorkSamples</h5>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam, delectus facere quidem modi dicta perspiciatis
+        illo harum quis aspernatur omnis beatae porro tempora est tenetur cupiditate eius, sequi suscipit.
+      </span>
+      <div className="samples-content">
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+        <div class="image-container">
+         
+          <img src="/assets/images/workSample.png" alt="workSample" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkSamples;
