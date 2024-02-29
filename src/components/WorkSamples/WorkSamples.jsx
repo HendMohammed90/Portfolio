fd2dc8ed6fe7f36d8cb9c6885ef335e368d4c1bd
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-29 23:37:10 +0200

    update work samples

--- src/components/WorkSamples/WorkSamples.jsx
+++ src/components/WorkSamples/WorkSamples.jsx
@@ -9,16 +9,13 @@ const WorkSamples = () => {
       Explore my work samples! Here, you'll find a variety of projects that showcase what I do best. From designs to completed work, see how I bring ideas to life.
       </span>
       <div className="samples-content">
-        <div className="image-container">
-         
-          <img src="/assets/images/workSample.png" alt="workSample" />
+      <div className="image-container">
+          <a href="https://www.squib.app/" target="blank"><img src="/assets/images/workSample2.png" alt="squibWebImg" /> </a>
         </div>
         <div className="image-container">
-         
-          <img src="/assets/images/workSample.png" alt="workSample" />
+          <a href="https://food-landing-page-gules.vercel.app/" target="blank"><img src="/assets/images/workSample1.png" alt="food-landing-page" /> </a>
         </div>
         <div className="image-container">
-         
           <img src="/assets/images/workSample.png" alt="workSample" />
         </div>
         <div className="image-container">
